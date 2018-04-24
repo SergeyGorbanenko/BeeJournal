298c89b9ebedb8c481521d64074dc6ca1d67cbeb
SergeyGorbanenko <xxx1607xxx@mail.ru>
2018-04-25 01:26:16 +0700

    Реализован рассчет статистики по работам: подсчет количества работ по каждому статусу за указанный период времени.

--- src/main/java/app/StatisticController.java
+++ src/main/java/app/StatisticController.java
@@ -1,10 +1,14 @@
 package app;
 
+import hba.WorkEntity;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class StatisticController {
@@ -31,10 +35,19 @@ public class StatisticController {
     }
 
     @FXML private Label lblTitle;
-    @FXML private ScrollPane scrlPane;
     @FXML private ComboBox<String> cmbRazdelName;
     @FXML private DatePicker dtDateStart;
     @FXML private DatePicker dtDateEnd;
+    //
+    @FXML private Pane workPane;
+    @FXML private Label lblCountPlan;
+    @FXML private Label lblCountProcess;
+    @FXML private Label lblCountDone;
+    @FXML private Label lblCountOverdue;
+    //
+    @FXML private Pane resursPane;
+    //
+    @FXML private Pane finansPane;
 
     private ObservableList<String> obserlistRazdelName;
 
@@ -55,12 +68,27 @@ public class StatisticController {
             if (dtDateStart.getValue() == null) throw new NullPointerException();
             if (dtDateEnd.getValue() == null) throw new NullPointerException();
             if (dtDateStart.getValue().isAfter(dtDateEnd.getValue())) throw new Exception();
-            if (cmbRazdelName.getValue().equals("Работы"))
+            if (cmbRazdelName.getValue().equals("Работы")) {
+                lblTitle.setText("Статистика по работам");
                 calculateWorkStatistic();
-            if (cmbRazdelName.getValue().equals("Ресурсы"))
+                workPane.setVisible(true);
+                resursPane.setVisible(false);
+                finansPane.setVisible(false);
+            }
+            if (cmbRazdelName.getValue().equals("Ресурсы")) {
+                lblTitle.setText("Статистика по ресурсам");
                 calculateResursStatistic();
-            if (cmbRazdelName.getValue().equals("Финансы"))
+                resursPane.setVisible(true);
+                workPane.setVisible(false);
+                finansPane.setVisible(false);
+            }
+            if (cmbRazdelName.getValue().equals("Финансы")) {
+                lblTitle.setText("Статистика по финансам");
                 calculateFinansStatistic();
+                finansPane.setVisible(true);
+                workPane.setVisible(false);
+                resursPane.setVisible(false);
+            }
         } catch (NullPointerException npe) {
             npe.printStackTrace();
             Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -78,14 +106,33 @@ public class StatisticController {
         }
     }
 
+    //Рассчет статистики по работам
     public void calculateWorkStatistic() {
-
+        int countPlan = 0;
+        int countProcess = 0;
+        int countDone = 0;
+        int countOverdue = 0;
+        Collection<WorkEntity> workEntityCollection = mainController.loadWorkList();
+        for (WorkEntity wrkE : workEntityCollection) {
+            if (wrkE.getDateStart().isAfter(dtDateStart.getValue()) && wrkE.getDateEnd().isBefore(dtDateEnd.getValue())) {
+                if (wrkE.getWorkStatus().equals("Планируется")) countPlan++;
+                if (wrkE.getWorkStatus().equals("В процессе")) countProcess++;
+                if (wrkE.getWorkStatus().equals("Выполнена")) countDone++;
+                if (wrkE.getWorkStatus().equals("Просрочена")) countOverdue++;
+            }
+        }
+        lblCountPlan.setText(String.valueOf(countPlan));
+        lblCountProcess.setText(String.valueOf(countProcess));
+        lblCountDone.setText(String.valueOf(countDone));
+        lblCountOverdue.setText(String.valueOf(countOverdue));
     }
 
+    //Рассчет статистики по ресурсам
     public void calculateResursStatistic() {
 
     }
 
+    //Рассчет статистики по финансам
     public void calculateFinansStatistic() {
 
     }
